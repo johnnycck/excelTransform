e4324437fa424f33d11b505ef25debbb5fb98bda
johnnycck <cckcsie@gmail.com>
2022-01-14 13:19:26 +0800

    store miss dir name

--- excelTransform.py
+++ excelTransform.py
@@ -12,12 +12,16 @@ files = os.listdir(path)
 input_xlsx = pd.read_excel('input.xlsx')
 input = input_xlsx.values
 target = []
+target_name = []
 target.append(1)
+target_name.append(1)
 n_rows, n_cols = input.shape
 for i in range(0,n_rows):
     target[i] = input[i][1]
+    target_name[i] = 0
     if i != n_rows-1:
         target.append(1)
+        target_name.append(1)
 path = path + '\\source'
 for root, dir, file in os.walk(path):
     if os.path.basename(root) == '.git':
@@ -34,3 +38,18 @@ for root, dir, file in os.walk(path):
                         s_path = root + '\\' + f
                         t_file = t_path + '\\' + f
                         shutil.copy(s_path, t_file)
+                    target_name[i] = 1
+wb = Workbook()
+ws = wb.active
+ws.cell(row = 1, column = 1, value = "test")
+ws.cell(row = 2, column = 1, value = "missing dir name")
+cur_row = 3
+for i in range(0, n_rows):
+    if(target_name[i] == 0):
+        ws.cell(row = cur_row, column = 1, value = target[i]) 
+        cur_row = cur_row+1
+        
+wb.save('tmp.xls')
+data_xls = pd.read_excel('tmp.xls',index_col=None)
+data_xls.to_csv('missing dir.csv', encoding='utf-8',sep=',',index=False,header=None)
+os.remove('tmp.xls')
