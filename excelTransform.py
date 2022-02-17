cd1e6a49efede27a355d3fa728297cd56803495f
johnnycck <cckcsie@gmail.com>
2022-02-17 14:00:09 +0800

    Find DIR with FID and ACQU

--- excelTransform.py
+++ excelTransform.py
@@ -16,6 +16,10 @@ target_name = []
 target.append(1)
 target_name.append(1)
 n_rows, n_cols = input.shape
+FALSE = 0
+TRUE = 1
+ifFID = TRUE
+curISD_name = 'tmp'
 for i in range(0,n_rows):
     target[i] = input[i][1]
     target_name[i] = 0
@@ -39,18 +43,28 @@ for root, dir, file in os.walk(path):
                     if (target[i] in f):
                         s_path = root + '\\' + f
                         t_file = t_path + '\\' + f
-                        print(f)
-                        al_copy = 0
-                        for iter_dir in range(0, n_dir):
-                            if (already_copy[iter_dir] == f):
-                                al_copy = 1
-                                break
-                        if(al_copy == 0):
-                            shutil.copytree(s_path, t_file)
-                            already_copy.append(1)
-                            already_copy[n_dir] = f
-                            n_dir = n_dir+1
-                        target_name[i] = 1
+                        ifFID = FALSE
+                        ifACQU = FALSE
+                        for inRoot, inDir, inFile in os.walk(s_path):
+                            if os.path.basename(inRoot) != s_path:
+                                for fi in inFile:
+                                    if(fi == 'fid'):
+                                        ifFID = TRUE
+                                    if(fi == 'acqu'):
+                                        ifFID = TRUE
+                        if(ifFID == TRUE and ifACQU == TRUE):
+                            al_copy = 0
+                            for iter_dir in range(0, n_dir):
+                                if (already_copy[iter_dir] == f):
+                                    al_copy = 1
+                                    break
+                            if(al_copy == 0):
+                                print(f)
+                                shutil.copytree(s_path, t_file)
+                                already_copy.append(1)
+                                already_copy[n_dir] = f
+                                n_dir = n_dir+1
+                            target_name[i] = 1
 wb = Workbook()
 ws = wb.active
 ws.cell(row = 1, column = 1, value = "test")
