77540bda8b0dca608fc71fe33e39ccb6d72cae67
johnnycck <cckcsie@gmail.com>
2022-08-18 14:39:16 +0800

    add new mode, extract all items from txt file

--- excelTransform.py
+++ excelTransform.py
@@ -5,6 +5,7 @@ import math
 import os
 import numpy as np
 import openpyxl
+import csv
 from openpyxl import Workbook
 from decimal import Decimal, ROUND_HALF_UP
 path = os.getcwd()
@@ -12,24 +13,31 @@ files = os.listdir(path)
 files_csv = [f for f in files if f[-3:] == 'txt']
 tmp_mz = 1.1
 print('number of files: '+str(len(files_csv)))
-
+limitMZ = 1 # extract range from 11300~12450
+noLimitMZ = 0 # extract all items in txt file
+# change the mode here, limitMZ/noLimitMZ
+mode = noLimitMZ
 for work_item in range (0,len(files_csv)):
     IO = files_csv[work_item]
     #print(IO)
     # 輸入的檔案名稱，需先轉成 xls 檔，並下移一行
     #IO = 'SAA_table_all.csv'
     sheet = pd.read_csv(IO,header=None,sep="\s+")
-    MZnum = 0;
-    print('---------- check number of m/z between 5000~12450 ----------')
+    underMZnum = 0
+    normalMZnum = 0
+    print('---------- check number of m/z before/after 11300 ----------')
     for i in range(0,len(sheet[0])):
-        if(sheet[0][i] >= 5000 and sheet[0][i] <=12450):
-            MZnum = MZnum + 1
-    print('MZnum: '+str(MZnum))
-    if(MZnum > 16384):
-        MZnum = 0;
+        if(sheet[0][i] < 11300):
+            underMZnum = underMZnum + 1
+        elif(sheet[0][i] >= 11300):
+            normalMZnum = normalMZnum + 1
+    print('normalMZnum: '+str(normalMZnum))
+    print('underMZnum: '+str(underMZnum))
+    if(mode == limitMZ):
+        MZnum = 0
         print('---------- check number of m/z between 11300~12450 ----------')
         for i in range(0,len(sheet[0])):
-            if(sheet[0][i] >= 11300 and sheet[0][i] <=12450):
+            if(sheet[0][i] >= 11300 and sheet[0][i] <= 12450):
                 MZnum = MZnum + 1
         if(MZnum > 16384):
             print('MZnum: '+str(MZnum))
@@ -71,8 +79,9 @@ for work_item in range (0,len(files_csv)):
             data_xls.to_csv(files_csv[work_item]+'.csv', encoding='utf-8',sep=',',index=False,header=None)
             os.remove('tmp.xls')
     else:
-        print('MZnum: '+str(MZnum))
         # 創建一個空白活頁簿物件
+        file = open(files_csv[work_item][:-4]+'.csv', 'w', newline='')
+        writer = csv.writer(file)
         wb = Workbook()
         # 選取正在工作中的表單
         ws = wb.active
@@ -80,30 +89,24 @@ for work_item in range (0,len(files_csv)):
         batch_name = IO.split('_')
         titles = ['All Patient Data', batch_name[0], IO[:-4]]
 
-        ws.cell(row = 1, column = 1, value = "test")
         print('---------- assign title ----------')
         # assign title
         for i in range(0,3):
+            #writer.writerow(titles[i])
             ws.cell(row = 2, column = i+1, value = titles[i])
             ws.cell(row = 3, column = i+1, value = titles[i])
             
         print('---------- start transposing content ----------')
         col_num = 4
         for i in range(3,len(sheet[0])+2):
-            if(sheet[0][i-3] >= 5000 and sheet[0][i-3] <=12450):
-                print(sheet[0][i-3])
-                print('dsd')
-                ws.cell(row = 2, column = col_num, value = round(sheet[0][i-3],1))
-                ws.cell(row = 3, column = col_num, value = sheet[1][i-3])
-                col_num = col_num+1
+            tmp_mz = Decimal(str(sheet[0][i-3])).quantize(Decimal('.1'), rounding=ROUND_HALF_UP)
+            ws.cell(row = 2, column = col_num, value = tmp_mz)
+            ws.cell(row = 3, column = col_num, value = sheet[1][i-3])
+            col_num = col_num+1
         print('file number '+str(work_item+1) +' is finished')
-            
-        # 儲存成 create_sample.xls 檔案
-        wb.save('tmp.xls')
-    
-        data_xls = pd.read_excel('tmp.xls',index_col=None)
-        if('.txt' in files_csv[work_item]):
-            files_csv[work_item] = files_csv[work_item][:-4]
-        data_xls.to_csv(files_csv[work_item]+'.csv', encoding='utf-8',sep=',',index=False,header=None)
-        os.remove('tmp.xls')
+        tmp_row = 0
+        for r in ws.rows:
+            if(tmp_row == 1 or tmp_row == 2):
+                writer.writerow([cell.value for cell in r])
+            tmp_row = tmp_row + 1
     work_item = work_item + 1
